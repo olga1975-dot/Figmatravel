234a93692e247d1a45a31f738bc65cf37cfae4f4
olga1975-dot <pentegova.olga@gmail.com>
2025-12-18 23:04:33 +0400

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,14 @@
 export default function App() {
   return (
-    <div>
-      <div className="bg-red-500 text-white p-10 text-3xl">
-        TAILWIND WORKS
+    <div className="min-h-screen bg-gradient-to-b from-[#0b132b] via-[#0f1c3f] to-[#020617] text-slate-100 flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold text-teal-400 mb-6">
+          Английский для путешествий
+        </h1>
+        <p className="text-lg text-slate-300 max-w-xl">
+          Современный онлайн-курс, который превращает английский язык
+          в удобный инструмент для реальных поездок.
+        </p>
       </div>
     </div>
   );
