05cc396d52e05462cc38f457b28f5e37f2a8dc28
olga1975-dot <pentegova.olga@gmail.com>
2025-12-18 23:13:05 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        bg1: "#0b132b",
-        bg2: "#0f1c3f",
-        bg3: "#020617",
-        accent: "#14b8a6",
-        accent2: "#22d3ee",
-        warm: "#f59e0b",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
