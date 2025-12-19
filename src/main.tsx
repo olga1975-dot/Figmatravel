7be596636dbf3d2a6dbb037fa521dff8b520c527
olga1975-dot <pentegova.olga@gmail.com>
2025-12-19 19:10:15 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
-import { createRoot } from "react-dom/client";
-import App from "./app/App";
-import "./styles/index.css"; // ❗ ОБЯЗАТЕЛЬНО
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './app/App'
+import './styles/index.css'   // ⬅⬅⬅ ВОТ ЭТОГО У ТЕБЯ НЕТ
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
